76688de0c28f577c4c1529ca5c67a480980336f8
Brandon <35798060+ETS5@users.noreply.github.com>
2018-10-06 11:04:52 -0500

    Update Hilux-restart.sh

--- Hilux-restart.sh
+++ Hilux-restart.sh
@@ -4,16 +4,11 @@
 #
 # Usage:
 # bash Hilux-setup.sh 
-#
-
 #Color codes
 RED='\033[0;91m'
 GREEN='\033[1;32m'
 YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
-
-
-
 #Delay script execution for N seconds
 function delay { echo -e "${GREEN}Sleep for $1 seconds...${NC}"; sleep "$1"; }
 
