ad45968bdeb6c7d31402e19f61740d0076ef2bec
Brandon <35798060+ETS5@users.noreply.github.com>
2018-10-06 06:01:05 -0500

    Add files via upload

---
+++ nodemon.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# Eswede nodemon 1.1 - hilux Masternode Monitoring
+#If you find this script helpful
+# ...please donate to the devfound: BTNX  **BMEfHRvfcwoPgpBgpEVERsQpipNjtFVgVL**
+# or BTC to **3H1JNkydHxDbhoXLREpxXccvyNh7Awr2jX**
+
+#...please donate BTNX to the devfound: BMEfHRvfcwoPgpBgpEVERsQpipNjtFVgVL
+
+#--ESWEDE
+
+#Processing command line params
+if [ -z $1 ]; then dly=1; else dly=$1; fi   # Default refresh time is 1 sec
+
+datadir="/home/$USER/.hiluxcore$2"   # Default datadir is /root/.hiluxcore
+ 
+# Install jq if it's not present
+dpkg -s jq 2>/dev/null >/dev/null || sudo apt-get -y install jq
+
+#It is a one-liner script for now
+watch -ptn $dly "echo '===========================================================================
+Outbound connections to other hilux nodes [hilux datadir: $datadir]
+===========================================================================
+Node IP               Ping    Rx/Tx     Since  Hdrs   Height  Time   Ban
+Address               (ms)   (KBytes)   Block  Syncd  Blocks  (min)  Score
+==========================================================================='
+hilux-cli -datadir=$datadir getpeerinfo | jq -r '.[] | select(.inbound==false) | \"\(.addr),\(.pingtime*1000|floor) ,\
+\(.bytesrecv/1024|floor)/\(.bytessent/1024|floor),\(.startingheight) ,\(.synced_headers) ,\(.synced_blocks)  ,\
+\((now-.conntime)/60|floor) ,\(.banscore)\"' | column -t -s ',' && 
+echo '==========================================================================='
+uptime
+echo '==========================================================================='
+echo 'Masternode Status: \n# hilux-cli masternode status' && hilux-cli -datadir=$datadir masternode status
+echo '==========================================================================='
+echo 'Sync Status: \n# hilux-cli mnsync status' &&  hilux-cli -datadir=$datadir mnsync status
+echo '==========================================================================='
+echo 'Masternode Information: \n# hilux-cli getinfo' && hilux-cli -datadir=$datadir getinfo
+echo '==========================================================================='
+echo 'Usage: nodemon.sh [refresh delay] [datadir index]'
+echo 'Example: nodemon.sh 10 22 will run every 10 seconds and query hiluxd in /$USER/.hiluxcore22'
+echo '\n\nPress Ctrl-C to Exit...'"
